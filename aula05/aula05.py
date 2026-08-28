ec1bf914ff82b8cc049950649a58659d89b2ff9b
samuellins <rnsamueldlins@gmail.com>
2026-08-28 11:50:12 -0300

    Create aula05.py

---
+++ aula05/aula05.py
@@ -0,0 +1,15 @@
+'''
+Dunder Methods() -> (Double Underline) Methods ()
+
+__str__() -> retorna, no objeto, diretamente na tela
+    print(g1)
+
+__getstate__() -> retorna dicionário personalizável
+
+Dunder Attributes
+
+__doc__ -> retorna documentação da classe
+__dict__ -> retorna dicionário
+__class__ -> retorna de qual classe é um objeto
+
+'''
