9ea518ee67b5612bd4de7b237359a9219ae33132
samuellins-ifrn <rnsamueldlins@gmail.com>
2026-09-02 10:17:42 -0300

    Create ex-circle.py

---
+++ atvs-sala-ifrn/ex-circle.py
@@ -0,0 +1,9 @@
+class Circle:
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = int(radius)
+
+    def point_in_circle = 
+
+circulo = Circle(150, 100, 75)
