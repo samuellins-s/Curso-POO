d8383515628d87a4ef1dfc8314d24688cae8736a
samuellins-ifrn <rnsamueldlins@gmail.com>
2026-09-16 15:33:35 -0300

    Update nivel01.py

--- atvs-sala-ifrn/lista 01/nivel01.py
+++ atvs-sala-ifrn/lista 01/nivel01.py
@@ -28,8 +28,20 @@ class Aluno:
     def __str__(self):
         return f'{self.nome} ({self.matricula}) -- {self.media()}'
 
-aluno1 = Aluno('Joabah', '23904723')
+aluno1 = Aluno('Keliomar da Costina', '23904723')
 aluno1.lancar_nota(6)
 
+aluno2 = Aluno('Conscrito Silvandro', '23195792')
+aluno2.lancar_nota(3)
+
+aluno3 = Aluno('Namaima do Corvado', '22064773')
+aluno3.lancar_nota(9)
+
 if aluno1.aprovado():
-    print(f'{aluno1} aprovado!!!')
+    print(f'Aluno(a) {aluno1} aprovado(a)!!!')
+
+if aluno2.aprovado():
+    print(f'Aluno(a) {aluno2} aprovado(a)!!!')
+
+if aluno3.aprovado():
+    print(f'Aluno(a) {aluno3} aprovado(a)!!!')
