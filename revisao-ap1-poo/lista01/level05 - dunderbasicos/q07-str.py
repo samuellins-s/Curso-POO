adbb7e4573bf825412b2fc8abe362d0b054c08d7
samuellins-ifrn <rnsamueldlins@gmail.com>
2026-09-24 14:10:53 -0300

    Create q07-str.py

---
+++ revisao-ap1-poo/lista01/level05 - dunderbasicos/q07-str.py
@@ -0,0 +1,39 @@
+class Retangulo:
+    def __init__(self, base: float, altura: float) -> None:
+
+        # validacao
+        if base <= 0:
+            raise ValueError('Base deve ser maior que zero!')
+        
+        if altura <= 0:
+            raise ValueError('Altura deve ser maior que zero!')
+
+        # passando da validacao, atribuir os valores
+        self.base = base
+        self.altura = altura
+
+    def __str__(self) -> str:
+        return f'Retângulo {self.base}x{self.altura}'
+    
+    def calcular_area(self) -> float:
+        return self.base * self.altura
+
+    def calcular_perimetro(self) -> float:
+        return 2 * (self.base + self.altura)
+
+    @classmethod
+    def quadrado(cls, lado: float) -> "Retangulo":
+        return cls(lado, lado)
+
+    @staticmethod
+    def eh_valido(base: float, altura: float) -> bool:
+        if base <= 0 or altura <=0:
+            return False
+        return True
+
+print(Retangulo.eh_valido(3, 4))
+print(Retangulo.eh_valido(-1, 4))
+print(Retangulo.eh_valido(-1, -9))
+
+r1 = Retangulo(3, 4)
+print(r1)
